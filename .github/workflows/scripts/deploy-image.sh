d41061af82ff63ef6c80aab89f00f4f9259a346f
shir fogel <shirf@sela.co.il>
2022-09-04 10:48:28 +0300

    checking scripts

--- .github/workflows/scripts/deploy-image.sh
+++ .github/workflows/scripts/deploy-image.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "script dir: $SCRIPT_DIR "
+ROOT_FOLDER=${GITHUB_WORKSPACE:=$SCRIPT_DIR/helm}
+echo "root folder: $ROOT_FOLDER "
+HELM_SERVICES_FOLDER="$ROOT_FOLDER/services"
+echo "services folder: $HELM_SERVICES_FOLDER "
+IMAGE_TAG_ANCHOR=${BRANCH_NAME:=master}
+echo "image folder: $IMAGE_TAG_ANCHOR "
