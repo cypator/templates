38b4420f529d00029e5d26f6cb82ddc862453f13
shir fogel <shirf@sela.co.il>
2022-09-05 11:27:08 +0300

    changing script

--- .github/workflows/scripts/deploy-image.sh
+++ .github/workflows/scripts/deploy-image.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# echo "script dir: $SCRIPT_DIR "
-# ROOT_FOLDER=${GITHUB_WORKSPACE:=$SCRIPT_DIR/devops}
-# echo "root folder: $ROOT_FOLDER "
-HELM_SERVICES_FOLDER="$GITHUB_WORKSPACE/devops/services"
-echo "services folder: $HELM_SERVICES_FOLDER "
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "script_dir: $SCRIPT_DIR"
+TARGET_ENV=${TARGET_ENV:=dev}
+echo "target_env: $TARGET_ENV"
+OUTPUT_PATH=${OUTPUT_PATH:=versions.command}
+echo "output_path: $OUTPUT_PATH"
+ROOT_FOLDER=${GITHUB_WORKSPACE:=$SCRIPT_DIR/devops}
+echo "root_folder: $ROOT_FOLDER"
+HELM_SERVICES_FOLDER="$ROOT_FOLDER/devops/services"
+echo "helm_service_folder: $HELM_SERVICES_FOLDER"
 IMAGE_TAG_ANCHOR=${BRANCH_NAME:=master}
-echo "image folder: $IMAGE_TAG_ANCHOR "
+echo "image_tag_anchor: $IMAGE_TAG_ANCHOR"
+touch $OUTPUT_PATH
+
+
 
 for dir in /$HELM_SERVICES_FOLDER/*/
 do
@@ -15,14 +22,36 @@ do
     REPO_NAME="$SERVICE_NAME"
     echo "SERVICE_NAME: $SERVICE_NAME"
     echo "REPO_NAME: $REPO_NAME"
-    # cmd="$(aws ecr describe-images --repository-name=$REPO_NAME --image-ids=imageTag=$IMAGE_TAG_ANCHOR)"
-    # if [ -z "$cmd" ]
+    # tag_list="$(aws ecr describe-images --repository-name=$REPO_NAME --region us-east-1 --image-ids=imageTag=$IMAGE_TAG_ANCHOR 2> /dev/null )"
+    # image_tags=$(echo $tag_list | jq -r '.imageDetails[0].imageTags' 2>&1)
+    # echo "tag_list: $tag_list"
+    # echo "image_tags: $image_tags"
+    
+    # echo ""  > found_tag
+    # while IFS= read -r line; do
+    #     re='^[0-9]+$'
+    #     line=$(echo "$line" | tr -d '"' | sed 's/,*$//' | xargs)
+    #     echo "checking tag: $line" 
+    #     if [[ $line =~ $re ]] ; then
+    #       echo "$line" > found_tag
+    #       echo "using: $line"
+    #       break;
+    #     fi
+    # done <<< "$image_tags"
+    
+    # FOUND_TAG=$(cat found_tag)
+    # if [ -z "$FOUND_TAG" ]
     # then
-    #     echo "This branch name doesn't exist in the repository - $REPO_NAME"
-    #     echo "::set-output name=image_exist::false"
-    #     exit 1
-    # else
-    #     echo "tag already exists in the repository - $REPO_NAME"
-    #     echo "::set-output name=image_exist::true"
+    #   FOUND_TAG=$IMAGE_TAG_ANCHOR
+    #   echo "using fallback tag"
     # fi
+    # echo "FOUND_TAG: $FOUND_TAG"
+    # if ! [ -z "$VERSIONS" ]
+    # then
+    #   VERSIONS+=" "
+    # fi
+
+    # VERSIONS+="-p $SERVICE_NAME.image.tag=$FOUND_TAG"
 done
+# echo "$VERSIONS" >> $OUTPUT_PATH
+# echo "VERSIONS=$VERSIONS" >> $GITHUB_ENV
