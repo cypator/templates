b4a825adb96ca5b568dfc74049a26ca545648da9
shir fogel <shirf@sela.co.il>
2022-09-07 11:26:54 +0300

    check secret script

--- .github/workflows/scripts/secrets-creator.py
+++ .github/workflows/scripts/secrets-creator.py
@@ -10,3 +10,15 @@ secrets_file=os.getenv('SECRETS_FILE_PATH',os.path.join(""))
 print(f"secrets_template: {secrets_file}")
 secrets_template=os.getenv('SECRETS_TEMPLATE_FILE_PATH',os.path.join(""))
 print(f"secrets_template: {secrets_template}")
+
+Path(output_folder).mkdir(parents=True, exist_ok=True)
+secrets = json.load(open(secrets_file))
+for secret in secrets:
+    secret_name=secret["name"]
+    secret_template = json.load(open(secrets_template))
+    secret_template["metadata"]["name"]=secret_name
+    for secret_entry in secret["data"]:
+        secret_template["data"][secret_entry]=secret["data"][secret_entry]
+    output_file=os.path.join(output_folder,f"{secret_name}-secret.json")
+    with open(output_file, 'w') as outfile:
+        json.dump(secret_template, outfile)
